baf88d1e77c6507d2c3c35cb3b5a3c8661f37d3f
Faraz909 <farazahmad0090@gmail.com>
2024-01-10 17:10:27 +0500

    this is not responsive

---
+++ typesc/index.js
@@ -0,0 +1,4 @@
+var fruit = ["apple", 'mangos'];
+console.log(fruit);
+var newfruit = fruit.pop();
+console.log(fruit);
---
+++ typesc/index.ts
@@ -0,0 +1,10 @@
+// const fruit:string[]=["apple" 'mangos'];
+// console.log(fruit);
+// const newfruit=fruit.pop();
+// console.log(fruit);
+
+
+
+
+
+
